e4f34f782775afdbadbb6a8e499d705d45eaa889
Abdur Rahim <rahim11500000@gmail.com>
2024-11-01 20:34:38 +0600

    added progress bar

--- src/components/Progress/Progress.jsx
+++ src/components/Progress/Progress.jsx
@@ -1,5 +1,6 @@
 import { BiSolidBarChartSquare, BiSolidDashboard } from "react-icons/bi";
 import { FaPause } from "react-icons/fa";
+import "./progress.css"
 
 const projects = [
   {
@@ -35,11 +36,36 @@ const projects = [
 
 ]
 const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",];
+const parseDate = (dateStr) => {
+  const [day, month, year] = dateStr.split('-').map(Number);
+  return new Date(2000 + year, month, day);
+};
 
 export default function Progress() {
-  const months = projects.map(project => parseInt(project.startDate.slice(3, 5)));
-  const minMonth = Math.min(...months)
-  const maxMonth = Math.max(...months)
+  const minMonth = Math.min(...projects.map(project => parseInt(project.startDate.slice(3, 5))));
+  const maxMonth = Math.max(...projects.map(project => parseInt(project.endDate.slice(3, 5))));
+
+  const startMonth = new Date(2023, minMonth, 1);
+  const endMonth = new Date(2023, maxMonth, 30);
+
+  const totalDays = Math.ceil((endMonth - startMonth) / (1000 * 60 * 60 * 24));
+  const totalWeeks = Math.floor(totalDays / 7);
+  console.log(totalDays, totalWeeks)
+
+  const progressOuterStyle = (startDate, endDate) => {
+    const projectStart = parseDate(startDate);
+    console.log(projectStart)
+    const projectEnd = parseDate(endDate);
+    const startOffset = Math.ceil((projectStart - startMonth) / (1000 * 60 * 60 * 24));
+    const projectDuration = Math.ceil((projectEnd - projectStart) / (1000 * 60 * 60 * 24));
+
+    return {
+      position: "absolute",
+      left: `${(startOffset / totalDays) * 100}%`,
+      width: `${(projectDuration / totalDays) * 100}%`,
+    };
+  };
+
 
   return (
     <div className="p-8 rounded-2xl border-2 bg-white">
@@ -55,19 +81,49 @@ export default function Progress() {
           <BiSolidBarChartSquare className="text-2xl" />
         </div>
       </div>
-      <div className="border rounded-2xl grid grid-cols-10">
-        <div className="col-span-1 flex">
-          <span className="border-r p-4">#</span>
-          <h4 className="p-4">Name</h4>
+      <div className="border rounded-2xl">
+        <div className="flex items-center">
+          <div className="flex w-[12.5%]">
+            <span className="border-r p-4">#</span>
+            <h4 className="p-4">Name</h4>
+          </div>
+          <div className="w-full p-4">
+            <div className="grid grid-cols-11 w-full">
+              {
+                monthNames.map((month, idx) => {
+                  if (idx + 1 >= minMonth && idx + 1 <= maxMonth) {
+                    return <p key={idx} className="">{month}</p>
+                  }
+                })
+              }
+            </div>
+            <div className="flex justify-between">
+            {
+              Array(totalWeeks).fill().map((_, idx) => <p key={idx} className="border-l p-1">{idx+1}</p>)
+            }
+            </div>
+          </div>
         </div>
-        <div className="col-span-9 flex justify-between p-4">
-          {
-            monthNames.map((month, idx) => {
-              return <p key={idx}>{idx + 1 >= minMonth && idx + 1 <= maxMonth && month}</p>
-            })
-          }
-        </div>
+        {projects.map((project, idx) => (
+          <div key={idx} className="flex items-center">
+            <div className="flex w-[12.5%]">
+              <span className="border-r p-4">{idx + 1}</span>
+              <h4 className="p-4">{project.projectName}</h4>
+            </div>
 
+            <div className="w-full relative">
+            <div className="flex justify-between">
+            {
+              Array(totalWeeks).fill().map((_, idx) => <p key={idx} className="border-l px-1 h-full"> </p>)
+            }
+            </div>
+              <div style={progressOuterStyle(project.startDate, project.endDate)} className="flex gap-2 items-center" >
+                <progress className="progress w-full" value={project.progress} max="100"></progress>
+                <span>{project.progress}%</span>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
