6c22c7ff8e1e048caf0eab6f6cfb132cdaaaee63
Abdur Rahim <rahim11500000@gmail.com>
2024-11-04 20:48:18 +0600

    make dynamic shape for businessGoal and objectives

--- src/components/Structure/Structure.jsx
+++ src/components/Structure/Structure.jsx
@@ -84,7 +84,7 @@ const strategicThemes = [
 
 export default function Structure() {
     return (
-        <div className="bg-white p-10">
+        <div className="bg-white p-10 space-y-10">
             {
                 strategicThemes.map((theme, idx) => (
                     <div key={idx} className="flex">
@@ -93,25 +93,80 @@ export default function Structure() {
                                 <p className="p-2 rounded bg-green-50 text-green-600 font-semibold mb-2">10/10</p>
                                 <p className="p-2 rounded bg-slate-100 font-semibold text-center">82%</p>
                             </div>
-                            <h2 className="text-2xl font-semibold">{theme.projectName}</h2>
+                            <h2 className="text-2xl font-semibold px-4">{theme.projectName}</h2>
                         </div>
 
                         <div>
                             {
                                 theme.businessGoal?.map((businessGoalI, idx) => (
-                                    <div key={idx} className="flex">
-                                        <div className="grid grid-cols-2 w-24">
-                                            <div className="border-r border-black"></div>
-                                            <div className="border-b border-l border-black rounded-bl-full -ml-[1px]"></div>
-                                            <div className="border-r border-black"></div>
-                                            <div className=""></div>
-                                        </div>
-                                        <div className="h-max p-2 border border-slate-600 rounded-xl flex items-center gap-4">
-                                            <div>
-                                                <p className="p-2 rounded bg-green-50 text-green-600 font-semibold mb-2">{businessGoalI.score}</p>
-                                                <p className="p-2 rounded bg-slate-100 font-semibold text-center">{businessGoalI.progress}%</p>
+                                    <div key={idx} className="flex relative">
+                                        <div className="flex">
+                                            {
+                                                idx === 0 ? <div className="grid grid-cols-2 w-24 -mt-12">
+                                                    <div className="border-b border-black"></div>
+                                                    <div className="border-b border-black -ml-[1px]"></div>
+                                                    <div className="border-r border-t border-black rounded-tr-3xl"></div>
+                                                    <div className=""></div>
+                                                </div> :
+                                                    idx === theme.businessGoal.length - 1 ? <div className="grid grid-cols-2 w-24 -mt-12">
+                                                        <div className=""></div>
+                                                        <div className="border-b border-l border-black rounded-bl-3xl -ml-[1px]"></div>
+                                                        <div className=""></div>
+                                                        <div className=""></div>
+                                                    </div> :
+                                                        <div className="grid grid-cols-2 w-24 -mt-12">
+                                                            <div className="border-r border-black"></div>
+                                                            <div className="border-b border-l border-black rounded-bl-full -ml-[1px]"></div>
+                                                            <div className="border-r border-black"></div>
+                                                            <div className=""></div>
+                                                        </div>
+                                            }
+                                            <div className="pb-12">
+                                                <div className="h-max p-2 border border-slate-600 rounded-xl flex items-center gap-4">
+                                                    <div>
+                                                        <p className="p-2 rounded bg-green-50 text-green-600 font-semibold mb-2">{businessGoalI.score}</p>
+                                                        <p className="p-2 rounded bg-slate-100 font-semibold text-center">{businessGoalI.progress}%</p>
+                                                    </div>
+                                                    <h2 className="text-2xl font-semibold px-4">{businessGoalI.title}</h2>
+                                                </div>
                                             </div>
-                                            <h2 className="text-2xl font-semibold">{businessGoalI.title}</h2>
+                                        </div>
+                                        <div className="absolute left-[99%] w-full">
+                                            {
+                                                businessGoalI.objectives?.map((objective, idx) => (
+                                                    <div key={idx} className="flex">
+                                                        {
+                                                            idx === 0 ? <div className="grid grid-cols-2 w-24 -mt-12">
+                                                                <div className="border-b border-black"></div>
+                                                                <div className="border-b border-black -ml-[1px]"></div>
+                                                                <div className="border-r border-t border-black rounded-tr-3xl"></div>
+                                                                <div className="border-l border-t border-black rounded-tl-3xl -ml-[1px]"></div>
+                                                            </div> :
+                                                                idx === businessGoalI.objectives.length - 1 ? <div className="grid grid-cols-2 w-24 -mt-12">
+                                                                    <div className=""></div>
+                                                                    <div className="border-b border-l border-black rounded-bl-3xl -ml-[1px]"></div>
+                                                                    <div className=""></div>
+                                                                    <div className=""></div>
+                                                                </div> :
+                                                                    <div className="grid grid-cols-2 w-24 -mt-12">
+                                                                        <div className="border-r border-black"></div>
+                                                                        <div className="border-b border-l border-black rounded-bl-full -ml-[1px]"></div>
+                                                                        <div className="border-r border-black"></div>
+                                                                        <div className=""></div>
+                                                                    </div>
+                                                        }
+                                                        <div className="pb-12">
+                                                            <div className="h-max p-2 border border-slate-600 rounded-xl flex items-center gap-4">
+                                                                <div>
+                                                                    <p className="p-2 rounded bg-green-50 text-green-600 font-semibold mb-2">{objective.score}</p>
+                                                                    <p className="p-2 rounded bg-slate-100 font-semibold text-center">{objective.progress}%</p>
+                                                                </div>
+                                                                <h2 className="text-2xl font-semibold px-4">{objective.title}</h2>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                ))
+                                            }
                                         </div>
                                     </div>
                                 ))
